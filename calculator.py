e1604f6fb00e42c00c7949bd98fbebed05f4c345
CADEO <cadewedekind@ufl.edu>
2025-04-14 20:54:47 -0400

    d

--- calculator.py
+++ calculator.py
@@ -28,7 +28,7 @@ def mul(a, b):
 
 def div(a, b):
     if b == 0:
-        return ZeroDivisionError("Error: Division by zero")
+        return ValueError("Error: Division by zero")
     return a / b
 
 def logarithm(b, a):
